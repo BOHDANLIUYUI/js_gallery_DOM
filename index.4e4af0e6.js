a0862f34d24e6deebc70bf97ddd876cebbe5aaa2
Bohdan Liutyi <liutyibohdan13@gmail.com>
2024-05-06 15:56:08 +0300

    make build

---
+++ index.4e4af0e6.js
@@ -0,0 +1,2 @@
+thumbs.onclick=function(t){"IMG"===t.target.tagName&&(largeImg.setAttribute("src",t.target.closest("a").href),largeImg.title=t.target.parentNode.title,t.preventDefault())};
+//# sourceMappingURL=index.4e4af0e6.js.map
